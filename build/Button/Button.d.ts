2c68a5b0085452552b34d9c14154522d21583a22
SaulMoreyra <armhacam21@gmail.com>
2022-05-04 21:35:27 -0500

    fix: json for publish

--- build/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export declare type ButtonProps = {
-    children: string | JSX.Element | JSX.Element[];
-    variant: "primary" | "secondary";
-};
-declare const Button: ({ variant, children, ...props }: ButtonProps) => JSX.Element;
-export default Button;
